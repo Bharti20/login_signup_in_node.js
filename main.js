944a67068f49052508022ea53ea6f99755ea7c6d
Bharti kumari <bhartikumari20@navgurukul.org>
2021-06-19 17:34:36 +0530

    complete task

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ else if(userInput == 'login') {
 
 function signUp() {
     let userData = {'user':[]}
-    let obj = {'username':"", 'password':""}
+    let obj = {'username':"", 'password':"", 'profile':""}
+    let bioData = {'description':"", 'dob':"", 'hobbies':"", 'gender':""}
     let userName = readlineSync.question('Enter your user name------')
     let password1 = readlineSync.question('Enter the password1 -------')
     let password2 = readlineSync.question('Enter the password2--------')
@@ -21,10 +22,23 @@ function signUp() {
                     if(jsData.length == 0) {
                         obj['username'] = userName
                         obj['password'] = password1
+                        // userData['user'].push(obj)
+                        // let allData = JSON.stringify(userData, null, 2)
+                        // fs.writeFileSync('userdetails.json',allData)
+                        console.log('congrats '+userName+" You are Signed Up Successfully")
+                        let descrip = readlineSync.question('Enter the description---')
+                        let dateOfBirth = readlineSync.question("Enter the date of birth ----")
+                        let hobbies = readlineSync.question('Enter the hobbies ----')
+                        let gender = readlineSync.question('Enter the gender-------')
+                        bioData['description'] = descrip
+                        bioData['dob'] = dateOfBirth
+                        bioData['hobbies'] = hobbies
+                        bioData['gender'] = gender
+                        obj['profile'] = bioData
                         userData['user'].push(obj)
                         let allData = JSON.stringify(userData, null, 2)
                         fs.writeFileSync('userdetails.json',allData)
-                        console.log('congrats'+userName+"You are Signed Up Successfully")
+
 
                     }else{
                         dataStore = readFile()
@@ -40,10 +54,22 @@ function signUp() {
                         }else{
                             obj['username'] = userName
                             obj['password'] = password1
+                            console.log()
+                            console.log('congrats '+userName+", You are Signed Up Successfully")
+                            console.log()
+                            let descrip = readlineSync.question('Enter the description---')
+                            let dateOfBirth = readlineSync.question("Enter the date of birth ----")
+                            let hobbies = readlineSync.question('Enter the hobbies ----')
+                            let gender = readlineSync.question('Enter the gender-------')
+                            console.log()
+                            bioData['description'] = descrip
+                            bioData['dob'] = dateOfBirth
+                            bioData['hobbies'] = hobbies
+                            bioData['gender'] = gender
+                            obj['profile'] = bioData
                             dataStore['user'].push(obj)
                             let allData = JSON.stringify(dataStore, null, 2)
                             fs.writeFileSync('userdetails.json',allData)
-                            console.log('congrats '+userName+", You are Signed Up Successfully")
                         }
                                               
                     }
@@ -57,14 +83,12 @@ function signUp() {
     }else{
         console.log('both password are not same')
     }
-
 }
 function readFile() {
     let data = fs.readFileSync('userdetails.json')
     let convertedData = JSON.parse(data)
     return convertedData
 }
-
 function logIn() {
     readData = readFile()
     let userName = readlineSync.question('Enter the username------')
@@ -73,16 +97,27 @@ function logIn() {
     while(index<readData['user'].length) {
         var d = readData['user'][index]['username']
         if(d == userName) {
+            var name = d
+            var sex = readData['user'][index]['profile']['gender']
+            var Bio = readData['user'][index]['profile']['description']
+            var hobb = readData['user'][index]['profile']['hobbies']
+            var birth = readData['user'][index]['profile']['dob']
             var p = readData['user'][index]['password']
             if(p == password) {
                 break
-            }else{
-                console.log("Invalid username and Password")
             }
-        
         }index++
     }if(d == userName) {
+        console.log()
         console.log('congrats '+userName+" You are Logged In Successfully")
+        console.log()
+        console.log('Profile:')
+        console.log('username: '+name)
+        console.log('Gender: '+sex)
+        console.log('Bio: '+Bio)
+        console.log('Hobbies: '+hobb)
+        console.log('Dob: '+birth)
+
     }else{
         console.log("Invalid username and Password")
     }
